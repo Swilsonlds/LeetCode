4cb94f56bee10c7162f324fc2c774cf9a2934ccc
Swilsonlds <91799590+Swilsonlds@users.noreply.github.com>
2023-08-25 12:44:17 -0700

    climbing stairs

--- Easy/ClimbingStairs/ClimbingStairs.py
+++ Easy/ClimbingStairs/ClimbingStairs.py
@@ -2,4 +2,9 @@
 # Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
 
 def climbingStairs(target: int) -> int:
+    if target < 2:
+        return 1
+    else:
+        return climbingStairs(target-1) + climbingStairs(target-2)
     
+print(climbingStairs(5))
